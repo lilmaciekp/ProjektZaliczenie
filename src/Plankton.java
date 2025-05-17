6c86586dc3cfcd3390cd97d4c698aa51aa018bc6
lil_maciek <f.maciej1998@gmail.com>
2025-05-17 21:11:53 +0200

    Update Plankton

--- src/Plankton.java
+++ src/Plankton.java
@@ -2,13 +2,25 @@ public class Plankton {
     private int energy;
     private int maxEnergy;
 
+    public Plankton(int maxEnergy) {
+        this.maxEnergy = maxEnergy;
+        this.energy = maxEnergy;
+    }
     public void regenerate(){
-
+        if(energy < maxEnergy){
+            energy++;
+        }
     }
     public boolean isDepleated(){
-        return false;
+        return energy <=0;
     }
     public void consume(int amount){
-
+        energy = Math.max(0, energy - amount);
+    }
+    public int getEnergy(){
+        return energy;
+    }
+    public int getMaxEnergy(){
+        return maxEnergy;
     }
 }
