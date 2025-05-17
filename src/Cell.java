aa12d5f57f1c127def9a70cdd01818246e2ce022
lil_maciek <f.maciej1998@gmail.com>
2025-05-17 21:12:37 +0200

    Update Cell

--- src/Cell.java
+++ src/Cell.java
@@ -2,25 +2,43 @@ public class Cell {
     private int x;
     private int y;
     private Fish fish;
-
     public Plankton plankton;
 
-    public boolean containsFish(){
-        return fish != null;
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.fish = null;
+        this.plankton = null;
+    }
+
+    public boolean isEmpty() {
+        return fish == null;
     }
     public Fish getFish(){
         return fish;
     }
-    public boolean containsHerbivorousFish(){
-        return false;
-    }
-    public HerbivorousFish getHerbivorousFish(){
-        return null;
-    }
-    public boolean isEmpty(){
-        return false;
-    }
     public void setFish(Fish fish){
         this.fish = fish;
+    }
+    public boolean containsHerbivorousFish(){
+        return fish instanceof HerbivorousFish;
+    }
+    public Plankton getPlankton(){
+        return plankton;
+    }
+    public HerbivorousFish getHerbivorousFish(){
+        if (fish instanceof HerbivorousFish) {
+            return (HerbivorousFish) fish;
+        }
+        return null;
+    }
+    public void setPlankton(Plankton plankton) {
+        this.plankton = plankton;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
     }
 }
