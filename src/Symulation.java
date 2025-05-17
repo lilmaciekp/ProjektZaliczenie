72aa421977a5d00f0eaa9163af18ddacc79a9b34
lil_maciek <f.maciej1998@gmail.com>
2025-05-17 19:34:17 +0200

    Usuniecie starej klasy

--- src/Symulation.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-import java.util.ArarayList;
-public class Symulation {
-    private Grid grid;
-    private Lis
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
